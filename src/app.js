5049092256f4f9c08dc103deff9c081b3b0365bc
DiegoKoyama <yukioko.jp@gmail.com>
2023-01-14 19:39:11 -0300

    funcao post/messages completa

--- src/app.js
+++ src/app.js
@@ -56,6 +56,35 @@ app.get("/participants", async (req, res) => {
     }
 })
 
+app.post("/messages", async (req, res) =>{
+    const message = req.body
+    const from = req.headers.user
+
+    const schema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.string().required()
+    })
+
+    const validation = schema.validate(message, { abortEarly: false })
+
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const resp = await db.collection("participants").findOne({ name: from })
+
+        if(!resp) return res.sendStatus(422)
+
+        await db.collection("messages").insertOne({ from: from, to: message.to, text: message.text, type: message.type, time: dayjs().format("HH:mm:ss")})
+        res.sendStatus(201)
+    } catch {
+        res.sendStatus(422)
+    }
+})
+
 
 
 
