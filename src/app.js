4962f02b731e77f9b7f0c7378cd9ec59eb7b4c28
DiegoKoyama <yukioko.jp@gmail.com>
2023-01-14 11:35:49 -0300

    funcionalidade post/participants implementado

--- src/app.js
+++ src/app.js
@@ -1,10 +1,44 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
+import joi from "joi";
+import dotenv from "dotenv";
+dotenv.config()
 
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+let db;
+
+try {
+    await mongoClient.connect()
+    db = mongoClient.db()
+} catch(err){
+    res.status(500).send(err.message)
+}
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.post("/participants", async (req, res) => {
+    const name = req.body
+
+    const nameSchema = joi.object({
+        name: joi.string().required()
+    })
+
+    const validation = nameSchema.validate(name)
+
+    try {
+        const resp = await db.collection("participants").findOne({name: name.name})
+
+        if(resp) return res.status(409).send("Participante já existe")
+
+        await db.collection("participants").insertOne({name: name.name})
+        res.send("ok")
+    } catch(err){
+        return res.status(500).send(err.message)
+    }
+})
 
 
 
